4433b8220e1575583a9aeefb30c0ba0c8e5a2095
Yugank Singh <badbudarsingh@gmail.com>
2021-07-09 16:24:56 +0530

    push project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+	<h1>H</h1>
+	<TodoList/>
+</div>
+	
+</template>
+
+<script>
+import TodoList from "./components/TodoList.vue"
+export default {
+	name: "App", 
+	components: {
+		TodoList
+	}
+}
+</script>
+
+<style>
+body{
+	background-color: #666666;
+	color: #ffffff;
+}
+</style>
---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,26 @@
+<template>
+<h1>Hello</h1>
+	<form @submit.prevent="addTodo()" class="col12 col-lg-6">
+		<input v-model="newTodoString " placeholder="What To-Do now" type="text" class="form-control">
+	</form>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			newTodoString: "",
+		}
+	},
+	methods: {
+		addTodo() {
+			if (this.newTodoString.length > 1) {
+				this.$emit("on-create", this.newTodoString)
+				this.newTodoString = ""
+			}
+		},
+	},
+}
+</script>
+
+<style scoped></style>
